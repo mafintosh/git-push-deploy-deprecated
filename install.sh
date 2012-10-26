705f2e081983c39a36ba878b6580402aec67facf
mafintosh <mathiasbuus@gmail.com>
2012-10-27 00:19:32 +0200

    added install script

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-append-to () {
+append () {
 	[ -e ~/.$1 ] && grep -v "## gpd " ~/.$1 > /dev/null && cat ~/.gpd/bashrc >> ~/.$1
 }
 
-append-to bashrc
-append-to bash_profile
+append bashrc
+append bash_profile
+
+git clone git@github.com:mafintosh/gpd.git ~/.git
