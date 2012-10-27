58695c32d87cc7270684e4bde72a0aa3e4a8ac4f
mafintosh <mathiasbuus@gmail.com>
2012-10-27 05:25:00 +0200

    typo in destination

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ append () {
 	[ -e ~/.$1 ] && grep -v "## gpd " ~/.$1 > /dev/null && cat ~/.gpd/bashrc >> ~/.$1
 }
 
-git clone git@github.com:mafintosh/gpd.git ~/.git
+git clone git@github.com:mafintosh/gpd.git ~/.gpd
 
 append bashrc
 append bash_profile
